8d473ca7215ef4ba9202e6be5f6acc4a9cf3b8bb
baysory <Eduardosantospereira14@gmail.com>
2025-07-11 06:02:58 -0300

    atualização breve, colocando o video de abertura e comprimindo ele na paginda dist, instalação do FFmpeg e gulp-fluent-ffmpeg

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,24 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const uglify = require('gulp-uglify');
+const terser = require('gulp-terser');
+const ffmpeg = require('gulp-fluent-ffmpeg');
 
 function scripts() {
     return gulp.src('./src/scripts/*.js')
-        .pipe(uglify())
+        .pipe(terser())
         .pipe(gulp.dest('./dist/js'));
+}
+
+function videos() {
+    return gulp.src('./src/videos/*.*')
+        .pipe(ffmpeg('mp4', function(cmd) {
+            return cmd.addOption('-crf', '28');
+        }))
+        .on('error', function(err) {
+            console.error('FFMPEG Error:', err.message);
+            this.emit('end'); // Evita que o Gulp quebre em caso de erro
+        })
+        .pipe(gulp.dest('./dist/videos'));
 }
 
 function styles() {
@@ -24,10 +37,10 @@ async function images() {
 exports.scripts = scripts;
 exports.styles = styles;
 exports.images = images;
+exports.videos = videos;
 
-exports.default = gulp.parallel(scripts, styles, images);
+exports.default = gulp.parallel(scripts, styles, images, videos);
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
     gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
 };
-
